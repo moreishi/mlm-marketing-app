6dabb5bc76c63ad11f4022d44ad58acc8413e23d
Caesar Ian Belza <moreishi@gmail.com>
2023-11-14 19:41:27 +0800

    develop add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(UserController::class)->group(function() {
+    Route::get('/users', 'index');
+    Route::post('/users', 'store');
+    Route::put('/users', 'update');
+    Route::delete('/users', 'delete');
+});
+
+Route::controller(AuthController::class)->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
 });
